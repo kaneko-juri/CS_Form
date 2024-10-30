ad842d523d379607b6b4fee75360ce69c6d49fa7
KJ <zeal24460221@fsgcl.jp>
2024-10-30 12:21:23 +0900

    TestButton.cs up date

---
+++ CS_Form/CS_Form/TestButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_Form
+{
+    internal class TestButton : Button
+    {
+        //<summary
+        //コンストラクタ
+        //クラスを生成したときに呼び出される
+        //</summary>
+
+        public TestButton()
+        {
+            //ClickイベントにOnclick関数を登録
+            Click += OnClick;
+
+        }
+
+        public void OnClick(object sender, EventArgs s)
+        {
+            MessageBox.Show("(*'▽')");
+        }
+    }
+}
